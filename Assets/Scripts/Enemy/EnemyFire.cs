4baf098e429184c162833423978919b9013822da
Minseong Seo <minsfor@gmail.com>
2026-09-03 03:11:11 +0900

    fix enemyFire

--- Assets/Scripts/Enemy/EnemyFire.cs
+++ Assets/Scripts/Enemy/EnemyFire.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemyFire : MonoBehaviour
@@ -6,10 +7,13 @@ public class EnemyFire : MonoBehaviour
     [SerializeField] GameObject projectilePrefab;
     [SerializeField] private float fireRanage = 3f;
     [SerializeField] private float projectileSpeed = 1f;
+    [SerializeField] private float fireDelay = 1f;
     private float lastfireTime = -999f;
 
     private Transform target = null;
+    private bool canFire = true;
 
+    private Vector3 targetDir;
     void OnEnable()
     {
         target = GameObject.FindGameObjectWithTag("Player").transform;
@@ -24,17 +28,33 @@ public class EnemyFire : MonoBehaviour
         curPos2D.y = 0;
         Vector3 targetPos2D = target.position;
         targetPos2D.y = 0;
+        targetDir = (targetPos2D - curPos2D).normalized;
+
         float dist = Vector3.Distance(curPos2D, targetPos2D);
         if (dist <= fireRanage && projectilePrefab != null)
         {
-            Vector3 dir = (targetPos2D - curPos2D).normalized;
-            GameObject projectileInstance = PoolManager.Instance.Get(PoolType.Projectile);
-            projectileInstance.transform.position = transform.position;
-            projectileInstance.transform.rotation = Quaternion.identity; ;
-            EnemyProjectile projectile = projectileInstance.GetComponent<EnemyProjectile>();
-            projectile.rb.linearVelocity = dir * projectileSpeed;
-            lastfireTime = Time.time;
+            if (canFire)
+            {
+                StartCoroutine(Fire());
+            }
         }
+
+    }
+
+    IEnumerator Fire()
+    {
+        canFire = false;
+
+        yield return new WaitForSeconds(fireDelay);
+
+        GameObject projectileInstance = PoolManager.Instance.Get(PoolType.Projectile);
+        projectileInstance.transform.position = transform.position;
+        projectileInstance.transform.rotation = Quaternion.identity; ;
+        EnemyProjectile projectile = projectileInstance.GetComponent<EnemyProjectile>();
+        projectile.rb.linearVelocity = targetDir * projectileSpeed;
+        lastfireTime = Time.time;
+
+        canFire = true;
     }
 
 }
