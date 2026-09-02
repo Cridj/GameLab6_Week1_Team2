8922f5fc26451d4e06f371c92247ec6213850e25
Minseong Seo <minsfor@gmail.com>
2026-09-02 22:29:38 +0900

    exclude navmesh

--- Assets/Scripts/Enemy/NeutralMovement.cs
+++ Assets/Scripts/Enemy/NeutralMovement.cs
@@ -1,48 +1,42 @@
 using UnityEngine;
-using UnityEngine.AI;
 
 public class NeutralMovement : MonoBehaviour
 {
     [SerializeField] private float updateInterval = 3f;
     [SerializeField] private float wanderingRadius = 5f;
+    [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private Animator animator;
-    private NavMeshAgent agent;
+
     private float timeSinceLastUpdate;
+    private Vector3 targetPosition;
 
     void OnEnable()
     {
-        agent = GetComponent<NavMeshAgent>();
         timeSinceLastUpdate = updateInterval;
+        targetPosition = GetRandomPosition();
     }
 
     void Update()
     {
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * moveSpeed);
         timeSinceLastUpdate += Time.deltaTime;
 
-        if (agent.isOnNavMesh && timeSinceLastUpdate >= updateInterval)
+        if (timeSinceLastUpdate >= updateInterval)
         {
-            Vector3 randomPosition = GetRandomPositionOnNavMesh();
-            agent.SetDestination(randomPosition);
+            targetPosition = GetRandomPosition();
             animator.Play("Hopak_Walk");
             timeSinceLastUpdate = 0f;
         }
 
     }
 
-    Vector3 GetRandomPositionOnNavMesh()
+    Vector3 GetRandomPosition()
     {
-        Vector3 randomDirection = Random.insideUnitSphere * wanderingRadius;
-        randomDirection += transform.position;
+        Vector2 randomCircle = Random.insideUnitCircle * wanderingRadius;
+        Vector3 pos = new Vector3(randomCircle.x + transform.position.x, 1, randomCircle.y + transform.position.z);
+        return pos;
 
-        NavMeshHit hit;
-        if (NavMesh.SamplePosition(randomDirection, out hit, wanderingRadius, NavMesh.AllAreas))
-        {
-            return hit.position;
-        }
-        else
-        {
-            return transform.position;
-        }
     }
 
 }
