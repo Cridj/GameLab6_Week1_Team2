8d6a56b82cbd379b45628eb348d3d67ad76b8b05
HotelPapaa <h980220@gmail.com>
2026-09-07 02:07:38 +0900

    260907

--- Assets/Scripts/Player/SpotLightMovement.cs
+++ Assets/Scripts/Player/SpotLightMovement.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class SpotLightMovement : MonoBehaviour
@@ -17,14 +18,23 @@ public class SpotLightMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ToCenter();
     }
+
 
     private void ToCenter()
     {
-        
+        int spot = 0;
+        leftSpotLight.transform.
     }
 
-
+    /*
+     * Spotlight 소환하기.(Instantiate) Unity에 강좌가 있던 것 같다.
+     * Instantiate (SpotLight Prefab으로 만들어서) -> 가운데 지점으로 이동. 가운데 지점에 이동하는 순간 비활성화.
+     * 가장 중요한 건 채보.. 
+     * 
+    
+     
+     */
 
 }
