02a315b6e3cab37e22f1c60879ff5488b5239bca
HotelPapaa <h980220@gmail.com>
2026-09-07 16:15:17 +0900

    feat: spotlight movement

--- Assets/Scripts/Player/SpotLightMovement.cs
+++ Assets/Scripts/Player/SpotLightMovement.cs
@@ -1,13 +1,20 @@
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpotLightMovement : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject leftSpotLight;
-    private GameObject rightSpotLight;
-    private GameObject frontSpotLight;
-    private GameObject backSpotLight;
+    [SerializeField] private GameObject leftSpotLight;
+
+    [SerializeField] private GameObject rightSpotLight;
+
+    [SerializeField] private GameObject frontSpotLight;
+
+    [SerializeField] private GameObject backSpotLight;
+
+    [SerializeField] private GameObject centerSpotLight;
+
+    [SerializeField] private float moveSpeed = 2f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,15 +25,37 @@ public class SpotLightMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ToCenter();
+        LeftToCenter();
+        RightToCenter();
     }
 
 
-    private void ToCenter()
+    private void LeftToCenter()
     {
-        int spot = 0;
-        leftSpotLight.transform.
+        if (leftSpotLight.transform.position.x >= centerSpotLight.transform.position.x)
+        {
+            leftSpotLight.SetActive(false);
+            return;
+        }
+
+        Vector3 position = leftSpotLight.transform.position;
+        position.x += moveSpeed * Time.deltaTime;
+        leftSpotLight.transform.position = position;
     }
+
+
+    private void RightToCenter()
+    {
+        if (rightSpotLight.transform.position.x <= centerSpotLight.transform.position.x)
+        {
+            rightSpotLight.SetActive(false);
+            return;
+        }
+        Vector3 position = rightSpotLight.transform.position;
+        position.x -= moveSpeed * Time.deltaTime;
+        rightSpotLight.transform.position = position;
+    }
+
 
     /*
      * Spotlight 소환하기.(Instantiate) Unity에 강좌가 있던 것 같다.
